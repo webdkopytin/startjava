c0ac6708aaeea9d4eed906bb07c0faf390401aa2
Dmitrii Kopytin <web@dkopytin.ru>
2021-07-13 23:16:28 +0300

    fix: отступы (задание 1)

--- Lesson001/Variable.java
+++ Lesson001/Variable.java
@@ -1,24 +1,24 @@
 public class Variable {
-	public static void main(String[] args) {
-		byte numberOfCores = 4;
-		short amountOfRam = 16;
-		int batteryChargePercent = 98;
-		long totalVideoMemoryVRam = 4;
-		float usbBusVersion = 3.1f;
-		double hardDiskCapacity = 499.96;
-		char firstLetterSerialNumber = 'C';
-		boolean  usedForStudying = true;
+    public static void main(String[] args) {
+        byte numberOfCores = 4;
+        short amountOfRam = 16;
+        int batteryChargePercent = 98;
+        long totalVideoMemoryVRam = 4;
+        float usbBusVersion = 3.1f;
+        double hardDiskCapacity = 499.96;
+        char firstLetterSerialNumber = 'C';
+        boolean  usedForStudying = true;
 
-		System.out.println(
-			"Информация о компьютере:" + System.lineSeparator() +
-			"\tчисло ядер: " + numberOfCores + System.lineSeparator() +
-			"\tоперативная память: " + amountOfRam + " Гб" + System.lineSeparator() +
-			"\tзаряд батареи: " + batteryChargePercent + "%" + System.lineSeparator() +
-			"\tразмер памяти видеокарты: " + totalVideoMemoryVRam + " Гб" + System.lineSeparator() +
-			"\tверсия USB: " + usbBusVersion  + System.lineSeparator() +
-			"\tсвободное место на диске: " + hardDiskCapacity + " Гб" + System.lineSeparator() +
-			"\tпервый символ серийного номера: " + firstLetterSerialNumber  + System.lineSeparator() +
-			"\tиспользуется для учёбы: " + usedForStudying  + System.lineSeparator()
-		);
-	}
+    System.out.println(
+        "Информация о компьютере:" + System.lineSeparator() +
+        "\tчисло ядер: " + numberOfCores + System.lineSeparator() +
+        "\tоперативная память: " + amountOfRam + " Гб" + System.lineSeparator() +
+        "\tзаряд батареи: " + batteryChargePercent + "%" + System.lineSeparator() +
+        "\tразмер памяти видеокарты: " + totalVideoMemoryVRam + " Гб" + System.lineSeparator() +
+        "\tверсия USB: " + usbBusVersion  + System.lineSeparator() +
+        "\tсвободное место на диске: " + hardDiskCapacity + " Гб" + System.lineSeparator() +
+        "\tпервый символ серийного номера: " + firstLetterSerialNumber  + System.lineSeparator() +
+        "\tиспользуется для учёбы: " + usedForStudying  + System.lineSeparator()
+        );
+    }
 }
