74d5ca55547237d5fc697a262dc0755b7846f34b
Dmitrii Kopytin <web@dkopytin.ru>
2021-07-13 23:23:44 +0300

    fix: отступы (задание 1)

--- Lesson001/Variable.java
+++ Lesson001/Variable.java
@@ -10,15 +10,15 @@ public class Variable {
         boolean  usedForStudying = true;
 
         System.out.println(
-            "Информация о компьютере:" + System.lineSeparator() +
-            "\tчисло ядер: " + numberOfCores + System.lineSeparator() +
-            "\tоперативная память: " + amountOfRam + " Гб" + System.lineSeparator() +
-            "\tзаряд батареи: " + batteryChargePercent + "%" + System.lineSeparator() +
-            "\tразмер памяти видеокарты: " + totalVideoMemoryVRam + " Гб" + System.lineSeparator() +
-            "\tверсия USB: " + usbBusVersion  + System.lineSeparator() +
-            "\tсвободное место на диске: " + hardDiskCapacity + " Гб" + System.lineSeparator() +
-            "\tпервый символ серийного номера: " + firstLetterSerialNumber  + System.lineSeparator() +
-            "\tиспользуется для учёбы: " + usedForStudying  + System.lineSeparator()
-            );
+                "Информация о компьютере:" + System.lineSeparator() +
+                "\tчисло ядер: " + numberOfCores + System.lineSeparator() +
+                "\tоперативная память: " + amountOfRam + " Гб" + System.lineSeparator() +
+                "\tзаряд батареи: " + batteryChargePercent + "%" + System.lineSeparator() +
+                "\tразмер памяти видеокарты: " + totalVideoMemoryVRam + " Гб" + System.lineSeparator() +
+                "\tверсия USB: " + usbBusVersion  + System.lineSeparator() +
+                "\tсвободное место на диске: " + hardDiskCapacity + " Гб" + System.lineSeparator() +
+                "\tпервый символ серийного номера: " + firstLetterSerialNumber  + System.lineSeparator() +
+                "\tиспользуется для учёбы: " + usedForStudying  + System.lineSeparator()
+        );
     }
 }
