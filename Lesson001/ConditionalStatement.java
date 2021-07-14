ab546c41cd51590acd4b7b5d5acd1d1a34444696
Dmitrii Kopytin <web@dkopytin.ru>
2021-07-15 00:27:37 +0300

    fix: название переменной hw_1.2

--- Lesson001/ConditionalStatement.java
+++ Lesson001/ConditionalStatement.java
@@ -1,14 +1,11 @@
 public class ConditionalStatement {
     public static void main(String[] args) {
         short age = 36;
-        boolean maleGender = true;
-        double height = 1.83;
-        char theFirstLetterName = 'D';
-
         if (age > 20) {
             System.out.println("Cтарше 20 лет");
         }
 
+        boolean maleGender = true;
         if (maleGender) {
             System.out.println("Мужчина");
         }
@@ -17,15 +14,17 @@ public class ConditionalStatement {
             System.out.println("Женщина");
         }
 
+        double height = 1.83;
         if (height < 1.80) {
             System.out.println("Рост менее 1 метр 80 сантиметров");
         } else {
             System.out.println("Рост более чем 1 метр 80 сантиметров");
         }
 
-        if (theFirstLetterName == 'M') {
+        char firstLetterName = 'D';
+        if (firstLetterName == 'M') {
             System.out.println("Первая буква имени: М");
-        } else if (theFirstLetterName == 'I') {
+        } else if (firstLetterName == 'I') {
             System.out.println("Первая буква имени: I");
         } else {
             System.out.println("Не удалось определить первую букву имени");
