225c513825f20fbc3e1ecc42246576c06e5d6b5a
Dmitrii Kopytin <web@dkopytin.ru>
2021-07-20 00:04:23 +0300

    add: hw - Unicode

---
+++ Lesson001/Unicode.java
@@ -0,0 +1,8 @@
+public class Unicode {
+    public static void main(String[] args) {
+        for (int i = 9398; i <= 10178; i++) {
+            char result = (char) i;
+            System.out.print(result + " ");
+        }
+    }
+}
