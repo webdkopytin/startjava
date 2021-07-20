7c6fc6240490deb8aaac92a23557c3967462a315
Dmitrii Kopytin <web@dkopytin.ru>
2021-07-20 23:31:39 +0300

    fix: hw_Unicode

--- Lesson001/Unicode.java
+++ Lesson001/Unicode.java
@@ -1,8 +1,7 @@
 public class Unicode {
     public static void main(String[] args) {
         for (int i = 9398; i <= 10178; i++) {
-            char result = (char) i;
-            System.out.print(result + " ");
+            System.out.print((char) i + " ");
         }
     }
 }
