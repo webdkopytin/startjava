3b113d5fac7da726daf992b144ec0e46fce3f078
Dmitrii Kopytin <web@dkopytin.ru>
2021-07-20 23:58:21 +0300

    fix: hw_MyFirstGame

--- Lesson001/MyFirstGame.java
+++ Lesson001/MyFirstGame.java
@@ -1,26 +1,24 @@
 public class MyFirstGame {
     public static void main(String[] args) {
         int hiddenNumber = 15;
-        int startNumber = 25;
+        int playerNumber = 18;
 
-        if (startNumber < hiddenNumber) {
-            for (int i = startNumber; i <= hiddenNumber; i++) {
+        if (playerNumber <= hiddenNumber) {
+            for (int i = playerNumber; i <= hiddenNumber; i++) {
                 if (i == hiddenNumber) {
                     System.out.println("Поздравляю, число (" + i + ") угадано!");
                 } else {
                     System.out.println("Данное число (" + i + ") меньше того, что загадал компьютер");
                 }
             }
-        } else if (startNumber > hiddenNumber) {
-            for (int i = startNumber; i >= hiddenNumber; i--) {
+        } else {
+            for (int i = playerNumber; i >= hiddenNumber; i--) {
                 if (i == hiddenNumber) {
                     System.out.println("Поздравляю, число (" + i + ") угадано!");
                 } else {
                     System.out.println("Данное число (" + i + ") больше того, что загадал компьютер");
                 }
             }
-        } else {
-            System.out.println("Поздравляю, число (" + hiddenNumber + ") угадано!");
         }
     }
 }
