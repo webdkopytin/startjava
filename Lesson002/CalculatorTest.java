85641f90b170dcaabd0b60f3e2960556b1fa5995
Dmitrii Kopytin <web@dkopytin.ru>
2021-07-23 16:26:37 +0300

    fix: logic hw_Calculator

--- Lesson002/CalculatorTest.java
+++ Lesson002/CalculatorTest.java
@@ -1,11 +1,29 @@
+import java.util.Scanner;
+
 public class CalculatorTest {
+
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        do {
-            int firstNumber = calculator.getFirstInt();
-            char operation = calculator.getOperation();
-            int secondNumber = calculator.getSecondInt();
-            calculator.calc(firstNumber, operation, secondNumber);
-        } while(calculator.getNextStep() == "yes");
+        Scanner scanner = new Scanner(System.in);
+
+        String question = "yes";
+
+        while(question.equals("yes")) {
+            System.out.print("Введите первое число: ");
+            calculator.setFirstInt(scanner.nextInt());
+
+            System.out.print("Введите знак математической операции: ");
+            calculator.setOperation(scanner.next().charAt(0));
+
+            System.out.print("Введите второе число: ");
+            calculator.setSecondInt(scanner.nextInt());
+
+            calculator.calc();
+
+            do {
+                System.out.print("Хотите продолжить вычисления? [yes/no]: ");
+                question = scanner.next();
+            } while (!question.equals("yes") && !question.equals("no"));
+        } 
     }
 }
