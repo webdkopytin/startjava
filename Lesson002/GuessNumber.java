285ced228e671e039a1658daba9a5b9dac0262d0
webdkopytin <web@dkopytin.ru>
2021-07-28 11:56:32 +0300

    fix: add logic game

--- Lesson002/GuessNumber.java
+++ Lesson002/GuessNumber.java
@@ -1,3 +1,43 @@
+import java.util.Scanner;
+
 public class GuessNumber {
-    
+    private Player playerOne;
+    private Player playerTwo;
+    private int compNumber;
+    private boolean isWin;
+    private Scanner scan = new Scanner(System.in);
+
+    public GuessNumber(Player playerOne, Player playerTwo) {
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
+    }
+
+    public void startGame() {
+        isWin = false;
+        compNumber = (int) (Math.random() * 100);
+        System.out.println("\nКомпьютер загадал число: " 
+        + compNumber);  
+        do {
+            inputUserNumber(playerOne);
+            guessingNumber(playerOne);
+            inputUserNumber(playerTwo);
+            guessingNumber(playerTwo);
+        } while (!isWin);
+    } 
+
+    public void inputUserNumber(Player player) {
+        System.out.print(player.getName() + ", введите число: ");
+        player.setNumber(scan.nextInt());
+    }
+
+    public void guessingNumber(Player player) {
+        if (player.getNumber() == compNumber) {
+            System.out.println(player.getName() + ", Вы угадали, это число: " + player.getNumber());
+            isWin = true;
+        } else if (player.getNumber() < compNumber) {
+            System.out.println(player.getName() + ", введенное Вами число меньше того, что загадал компьютер");
+        } else if (player.getNumber() > compNumber) {
+            System.out.println(player.getName() + ", введенное Вами число больше того, что загадал компьютер");
+        }
+    }
 }
--- Lesson002/GuessNumberTest.java
+++ Lesson002/GuessNumberTest.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class GuessNumberTest {
     public static void main (String[] args) {
-        System.out.println("!");
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Первый игрок, введите свое имя: ");
+        Player playerOne = new Player(scan.next());
+        
+        System.out.print("Второй игрок, введите свое имя: ");
+        Player playerTwo = new Player(scan.next());
+
+        GuessNumber game = new GuessNumber(playerOne, playerTwo);
+        
+        String response = "yes";
+        do {
+            game.startGame();
+
+            do {
+                System.out.print("Хотите продолжить? [yes/no]: ");
+                response = scan.next();
+            } while ((!response.equals("yes")) && (!response.equals("no")));
+
+        } while(response.equals("yes"));
+        System.out.println("Удачи!");
     }
 }
