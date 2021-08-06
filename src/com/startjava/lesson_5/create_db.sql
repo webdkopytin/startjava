3fd034a24762c7f80fdc2deaf86edea763eed939
webdkopytin <web@dkopytin.ru>
2021-08-06 18:45:19 +0300

    add: create database & table

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -1,2 +1,14 @@
 -- create db
-
+CREATE DATABASE Jaegers;
+-- create table jaegers
+CREATE TABLE jaegers (
+    id SERIAL PRIMARY KEY,
+    modelName TEXT,
+    mark CHAR(7),
+    height REAL,
+    weight REAL,
+    status CHAR(9),
+    origin TEXT,
+    launch DATE,
+    kaijuKill INTEGER
+);
