73544fa6157eb5bce3b61b5df92b8397a957f432
webdkopytin <web@dkopytin.ru>
2021-08-06 19:14:00 +0300

    add: info table jaegers

--- src/com/startjava/lesson_5/init_db.sql
+++ src/com/startjava/lesson_5/init_db.sql
@@ -1,2 +1,13 @@
 -- init db
-
+INSERT INTO jaegers (modelName, mark, height, weight, status, origin, launch, kaijuKill)
+    VALUES
+        ('Striker Eureka', 'Mark-5', '250', '1850', 'Destroyed', 'Australia', '20190902', '11'),
+        ('Bracer Phoenix', 'Mark-5', '232', '2128', 'Destroyed', 'Australia', '20250901', '0'),
+        ('Crimson Typhoon', 'Mark-4', '250', '1722', 'Destroyed', 'China', '20180822', '7'),
+        ('Saber Athena', 'Mark-7', '252', '1628', 'Destroyed', 'South Korea', '20331220', '1'),
+        ('Puma Real', 'Mark-2', '267.70', '1789', 'Destroyed', 'China', '20181026', '2'),
+        ('Gipsy Danger', 'Mark-3', '260', '1980', 'Destroyed', 'United States of America', '20170710', '6'),
+        ('Gipsy Avenger', 'Mark-6', '268', '2004', 'Destroyed', 'Japan', '20340112', '1'),
+        ('Diablo Intercept', 'Mark-2', '240', '2390', 'Worker', 'USA', '20160723', '9'),
+        ('Vulcan Specter', 'Mark-3', '210', '1945', 'Worker', 'Mexico', '20300612', '5'),
+        ('Brawler Yukon', 'Mark-1', '200', '1910', 'Worker', 'South Korea', '20200919', '3');
